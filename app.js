092e9f75971492859ff3bdf39bd7c83c3e2138ac
Clemens Bruesch <clemensbruesch@gmail.com>
2021-01-21 17:53:59 +0100

    function logAllCourses

--- app.js
+++ app.js
@@ -13,17 +13,32 @@ async function asyncFunction() {
 	conn = await pool.getConnection();
 	const rows = await conn.query("SELECT 1 as val");
 	console.log(rows); //[ {val: 1}, meta: ... ]
-	// const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-  // console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-  // const user = await conn.query("select user,host from mysql.user;")
-  const courses = await conn.query("SELECT name FROM courses;")
-  console.log(courses);
-
+	const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+  console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+  const user = await conn.query("select user,host from mysql.user;")
   } catch (err) {
 	throw err;
   } finally {
 	if (conn) return conn.end();
   }
-}
+};
 
-asyncFunction();
+async function logAllCourses () {
+  let conn;
+  try {
+  conn = await pool.getConnection();
+  const courses = await conn.query({rowsAsArray: true, sql: "SELECT name FROM courses;"})
+  courses.forEach(x => console.log(x[0]));
+  } catch (err) {
+	  throw err;
+  } finally {
+	  if (conn) return conn.end();
+  }
+};
+
+// asyncFunction();
+
+logAllCourses();
+
+
+// https://github.com/mariadb-corporation/mariadb-connector-nodejs/blob/master/documentation/promise-api.md#array-result-sets
