7723f68cf55a01354c00fc59f97f4b6cba9c95b0
Clemens Bruesch <clemensbruesch@gmail.com>
2021-01-21 14:47:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+     host: '127.0.0.1',
+     port: '3306',
+     user: 'clemens', 
+     database: 'codingschool',
+     password: '',
+     connectionLimit: 5
+});
+async function asyncFunction() {
+  let conn;
+  try {
+	conn = await pool.getConnection();
+	const rows = await conn.query("SELECT 1 as val");
+	console.log(rows); //[ {val: 1}, meta: ... ]
+	// const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+  // console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+  // const user = await conn.query("select user,host from mysql.user;")
+  const courses = await conn.query("SELECT name FROM courses;")
+  console.log(courses);
+
+  } catch (err) {
+	throw err;
+  } finally {
+	if (conn) return conn.end();
+  }
+}
+
+asyncFunction();
