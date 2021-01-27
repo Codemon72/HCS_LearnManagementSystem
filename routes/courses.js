f58ef5528e90fa4107ffdee0235dfd38881c2615
Clemens Bruesch <clemensbruesch@gmail.com>
2021-01-27 10:31:32 +0100

    manually inserted course via sequelize

--- routes/courses.js
+++ routes/courses.js
@@ -1,16 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../config/database');
-const Courses = require('../models/Courses');
+const db = require("../config/database");
+const Courses = require("../models/Courses");
 
-router.get('/', (req, res) => 
+// Get all courses
+router.get('/', (req, res) =>
   // res.send('all good so far 🏄‍♂️🥋')
   Courses.findAll()
-    .then(courses => {
+    .then((courses) => {
       console.log(courses);
       res.sendStatus(200);
     })
-    .catch(err => console.log(err))
-  );
+    .catch((err) => console.log(err))
+);
+
+// Add a course
+router.get('/add', (req, res) => {
+  const data = {
+    name: 'Vue.js',
+    hours: 24,
+    start_date: '2021-02-15',
+    end_date: '2021-03-03',
+    teacher_id: 9
+  };
+
+  let { name, hours, start_date, end_date, teacher_id } = data;
+
+  // Insert into table
+  Courses.create({
+    name, 
+    hours, 
+    start_date, 
+    end_date, 
+    teacher_id
+  })
+    .then(blib => res.redirect('/courses'))
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
