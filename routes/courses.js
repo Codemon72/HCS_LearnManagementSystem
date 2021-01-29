1ff3ecbf038e3c68fe0765cd912a06b67887a063
Clemens Bruesch <clemensbruesch@gmail.com>
2021-01-29 16:50:48 +0100

    form /courses/add inserts data to db

--- routes/courses.js
+++ routes/courses.js
@@ -23,27 +23,77 @@ router.get('/add', (req, res) => res.render('add'));
 
 // Add a course
 router.post('/add', (req, res) => {
-  const data = {
-    name: 'Vue.js',
-    hours: 24,
-    start_date: '2021-02-15',
-    end_date: '2021-03-03',
-    teacher_id: 9
-  };
+  let { name, hours, start_date, end_date, teacher_id } = req.body;
+  let errors = [];
 
-  let { name, hours, start_date, end_date, teacher_id } = data;
+  // Validate Fields
+  if(!name) {
+    errors.push({ text: 'Please add the course name' });
+  }
+  if(!hours) {
+    errors.push({ text: 'Please add the number of hours' });
+  }
+  if(!start_date) {
+    errors.push({ text: 'Please add the start date of the course' });
+  }
+  if(!end_date) {
+    errors.push({ text: 'Please add the end date of the course' });
+  }
+  if(!teacher_id) {
+    errors.push({ text: 'Please add the teacher id' });
+  }
 
-  // Insert into table
-  Courses.create({
-    // instead of 'name: name' with ES6 we can go like this:
-    name, 
-    hours, 
-    start_date, 
-    end_date, 
-    teacher_id
-  })
+  // Check for errors
+  if (errors.length > 0) {
+    res.render('add', {
+      errors,
+      name, 
+      hours, 
+      start_date, 
+      end_date, 
+      teacher_id
+    });
+  } else {
+    // Insert into table
+    Courses.create({
+      // instead of 'name: name' with ES6 we can go like this:
+      name, 
+      hours, 
+      start_date, 
+      end_date, 
+      teacher_id
+    })
     .then(blib => res.redirect('/courses'))
     .catch(err => console.log(err));
+  }
+
 });
 
 module.exports = router;
+
+
+// Hacky way to quickly insert a course to the database:
+
+// router.get('/add', (req, res) => {
+//   const data = {
+//     name: 'Vue.js',
+//     hours: 24,
+//     start_date: '2021-02-15',
+//     end_date: '2021-03-03',
+//     teacher_id: 9
+//   };
+
+//   let { name, hours, start_date, end_date, teacher_id } = data;
+
+//   // Insert into table
+//   Courses.create({
+//     // instead of 'name: name' with ES6 we can go like this:
+//     name, 
+//     hours, 
+//     start_date, 
+//     end_date, 
+//     teacher_id
+//   })
+//     .then(blib => res.redirect('/courses'))
+//     .catch(err => console.log(err));
+// });
