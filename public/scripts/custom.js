7b2a4eea41a3fd03973c74a3d5fe0de1a4b686c3
Clemens Bruesch <clemensbruesch@gmail.com>
2021-03-02 13:00:23 +0100

    custom script can access form element

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -1,1 +1,14 @@
 console.log('custom JS file connected');
+
+// const testFunction = () => {
+//   console.log('Success!')
+// }
+
+const nameField = document.getElementById('name');
+console.log(nameField)
+
+const myFunction = () => {
+  alert("Input field lost focus.");
+}
+
+// module.exports = myFunction;
