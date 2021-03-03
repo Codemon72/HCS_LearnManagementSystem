e496a9f3d05f5620a56fbd0ff8f4e0f9a091c45b
Clemens Bruesch <clemensbruesch@gmail.com>
2021-03-03 13:11:40 +0100

    added styling to error message, eventListener for change gets triggered

--- public/scripts/customJS.js
+++ public/scripts/customJS.js
@@ -1,13 +1,19 @@
-const nameField = document.getElementById("name");
-nameField.addEventListener("blur", () => {
+// Form Validation
+const nameField = document.getElementById('name');
+nameField.addEventListener('blur', () => {
   if (nameField.options[nameField.selectedIndex].value === ""){
-    nameField.insertAdjacentHTML("afterend", "<span class='error-message'>Please select a course name from the list.</span>");
+    nameField.insertAdjacentHTML('afterend', '<span class="error-message">Please select a course name from the list.</span>');
   }
 });
+nameField.addEventListener('change', () => {
+  console.log('success!')
+  // if valid input add check mark and disappear error message
+  // if invalid input && error-message exists -> do nothing
+});
 
-const hoursField = document.getElementById("hours");
-hoursField.addEventListener("blur", () => {
-  if (hoursField.value === ""){
+const hoursField = document.getElementById('hours');
+hoursField.addEventListener('blur', () => {
+  if (hoursField.value === ''){
     hoursField.insertAdjacentHTML("afterend", "<span class='error-message'>Please enter the sum of hours of the course.</span>");
   }
 });
