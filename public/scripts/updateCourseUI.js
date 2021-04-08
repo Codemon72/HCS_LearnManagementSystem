0253f0b1be6195bfbbfddcb7e592a3d5f5b29622
Clemens Bruesch <clemensbruesch@gmail.com>
2021-04-08 10:16:53 +0200

    clean-up

--- public/scripts/updateCourseUI.js
+++ public/scripts/updateCourseUI.js
@@ -2,9 +2,6 @@ const displayUpdateUI = (id) => {
   const editDiv = document.getElementById('editCourse' + id)
   editDiv.classList.remove('inactive');
   editDiv.classList.add('active');
-  // TODO
-  // if course_id === last-child add margin-bottom: 21rem !important to .container
-  // or: if course_id element === array of course-elements[-1]...
 }
 
 const cancelUpdate = (id) => {
